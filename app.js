24bfcb44ff7e5ab9aa1cd32122017ab827b52b4f
freeanser <bnn01615@gmail.com>
2023-07-15 11:43:40 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ app.use(methodOverride('_method'))
 
 usePassport(app) // 因為 passport 最後是輸出一個 function
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes)
 
 // 建立監聽器
